56d4639ff41253a148c014e157ce10e8982767dd
aj <aj@ajs-MacBook-Pro.local>
2026-02-02 13:29:04 -0500

    dataform updates

--- dataform/definitions/clinical/synthea/declarations.js
+++
@@ -1,5 +0,0 @@
-declare({
-  database: "prj-clin-syn-np",
-  schema: "synthea_bronze_dev",
-  name: "conditions",
-});
--- dataform/silver_conditions.sh
+++
@@ -1,6 +0,0 @@
-npx dataform run \
-  --project-dir . \
-  --environment dev \
-  --actions silver_conditions
-
-
